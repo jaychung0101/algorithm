ba5685144c8ef09553afeb846a731e8c192d0e15
Jihoon <144250180+jaychung0101@users.noreply.github.com>
2025-03-17 22:14:25 +0900

    [Gold V] Title: 회의실 배정, Time: 24 ms, Memory: 2804 KB -BaekjoonHub

---
+++ C++17/백준/Gold/1931. 회의실 배정/회의실 배정.cc
@@ -0,0 +1,43 @@
+// 회의실 배정
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool cmp(pair<int, int> a, pair<int, int> b){
+    if(a.first != b.first)
+        return a.first < b.first;
+    else
+        return a.second < b.second;
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    int N; cin >> N;
+    vector<pair<int, int>> v(N);
+    
+    int x, y; 
+    for(int i=0; i<N; i++){
+        cin >> x >> y;
+        v[i] = {x, y};
+    }
+
+    sort(v.begin(), v.end());
+
+    int res = 1;
+    int last = v[0].second;
+    for(int i=1; i<N; i++){
+        if(v[i].first >= last){
+            res++;
+            last = v[i].second;
+        }
+        if(v[i].second < last){
+            last = v[i].second;
+        }
+    }
+
+    cout << res;
+
+    return 0;
+}
