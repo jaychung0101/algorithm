92ab8afe82696378e61b4caa01d21fee00e09e02
Jihoon <144250180+jaychung0101@users.noreply.github.com>
2025-03-18 14:58:23 +0900

    [Silver III] Title: N과 M (5), Time: 32 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,41 @@
+// N과 M (5)
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+
+int ans[10];
+int list[10];
+bool used[10];
+
+void backTracking(int size){
+    if(size==M){
+        for(int i=0; i<M; i++) cout << ans[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    for(int i=0; i<N; i++){
+        if(used[i]==0){
+            used[i] = true;
+            ans[size] = list[i];
+            backTracking(size+1);
+            used[i] = false;
+        }
+    }
+
+    return;
+}
+
+int main(){
+    cin >> N >> M;
+
+    for(int i=0; i<N; i++) cin >> list[i];
+
+    sort(list, list+N);
+
+    backTracking(0);
+
+    return 0;
+}
