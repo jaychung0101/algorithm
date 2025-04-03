b853f02fb68a900457eb2e46c13c0e0ac0a511f7
Jihoon <144250180+jaychung0101@users.noreply.github.com>
2025-04-03 12:40:20 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.20 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ C++/프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<vector<int>> jobs) { // {요청 시간, 소요 시간}
+    int answer = 0, maxIdx = jobs.size();
+    map<int, 
+        priority_queue<
+            pair<int, int>, 
+            vector<pair<int, int>>, 
+            greater<pair<int, int>>
+            >
+        > eventQueue;    
+    
+    int currentTime = 0, idx = 0, processedJob = 0;
+    sort(jobs.begin(), jobs.end());
+    while(processedJob < maxIdx){
+        while(idx<maxIdx && jobs[idx][0]<=currentTime){
+            eventQueue[jobs[idx][1]].push({jobs[idx][0], idx});
+            idx++;
+        }
+        
+        if(idx<maxIdx && eventQueue.empty()){
+            currentTime = jobs[idx][0];
+            continue;
+        }
+        
+        auto it = eventQueue.begin();
+        int spendTime = it->first;
+        int requestTime = it->second.top().first; 
+        it->second.pop();
+
+        currentTime += spendTime;
+        answer += currentTime - requestTime;
+
+        if(it->second.empty()) eventQueue.erase(it);
+        
+        processedJob++;
+    }
+    
+    
+    return answer/maxIdx;
+}
