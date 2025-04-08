ad8f92ce011cf6844c34a1bbae3f28348f986e85
Jihoon <144250180+jaychung0101@users.noreply.github.com>
2025-04-08 15:07:50 +0900

    [Gold V] Title: 내려가기, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Gold/2096. 내려가기/내려가기.cc
+++ C++17/백준/Gold/2096. 내려가기/내려가기.cc
@@ -19,15 +19,15 @@ int main(){
         vector<int> temp2 = dp2;
 
         dp1[0] = next[0] + max(temp1[0], temp1[1]);
-        dp1[1] = next[1] + *max_element(temp1.begin(), temp1.end());
+        dp1[1] = next[1] + max({temp1[0], temp1[1], temp1[2]});
         dp1[2] = next[2] + max(temp1[1], temp1[2]);
 
         dp2[0] = next[0] + min(temp2[0], temp2[1]);
-        dp2[1] = next[1] + *min_element(temp2.begin(), temp2.end());
+        dp2[1] = next[1] + min({temp2[0], temp2[1], temp2[2]});
         dp2[2] = next[2] + min(temp2[1], temp2[2]);
     }
     
-    cout << *max_element(dp1.begin(), dp1.end()) << ' ' << *min_element(dp2.begin(), dp2.end());
+    cout << max({dp1[0], dp1[1], dp1[2]}) << ' ' << min({dp2[0], dp2[1], dp2[2]});
 
     return 0;
 }
