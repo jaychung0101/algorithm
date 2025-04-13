040c64bea4a7c2cac36da8b42405eafa29e58c63
Jihoon <144250180+jaychung0101@users.noreply.github.com>
2025-04-13 13:12:38 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 36 ms, Memory: 5728 KB -BaekjoonHub

---
+++ C++17/백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
@@ -0,0 +1,72 @@
+//특정한 최단 경로
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int N, E;
+int interDis;
+bool flag = true;
+vector<vector<pair<int, int>>> adj;
+
+pair<int, int> dijkstra(int u1, int u2){
+    pair<int, int> answer;
+
+    vector<bool> used(N+1, false);
+    vector<int> dist(N+1, INT32_MAX);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, u1});
+    dist[u1] = 0;
+
+    while(!pq.empty()){
+        int cV = pq.top().second; pq.pop();
+        
+        if(used[cV]) continue;
+        
+        if(cV==1) answer.first = dist[1];
+        else if(cV==N) answer.second = dist[N];
+        
+        if(cV==u2) interDis = dist[u2];
+
+        used[cV] = true;
+
+        for(auto n : adj[cV]){
+            int nW = n.first;
+            int nV = n.second;
+            if(used[nV]) continue;
+            if(dist[cV]+nW<dist[nV]){
+                dist[nV] = dist[cV] + nW;
+                pq.push({dist[nV], nV});
+            }
+        }
+    }
+
+    if(dist[1] == INT32_MAX || dist[N]==INT32_MAX || dist[u1]==INT32_MAX || dist[u2]==INT32_MAX) flag = false;
+    
+    return answer;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> N >> E;
+    adj.resize(N+1, vector<pair<int, int>>());
+    while(E--){
+        int u, v, w; cin >> u >> v >> w;
+        adj[u].push_back({w, v});
+        adj[v].push_back({w, u});
+    }    
+
+    int n1, n2; cin >> n1 >> n2;
+    pair<int, int> answer1 = dijkstra(n1, n2);
+    pair<int, int> answer2 = dijkstra(n2, n1);
+
+    if(!flag) cout << -1;
+    else {
+        cout << min(answer1.first+answer2.second, answer1.second+answer2.first) + interDis;
+    }
+    cout << '\n';
+
+    return 0;
+}
