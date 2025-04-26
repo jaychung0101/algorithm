5fa30303c9938faaed2cad2f27a77fbc671bec31
Jihoon <144250180+jaychung0101@users.noreply.github.com>
2025-04-26 22:08:38 +0900

    [Platinum IV] Title: 문명, Time: 732 ms, Memory: 46252 KB, Score: 100 point -BaekjoonHub

--- C++17/백준/Platinum/14868. 문명/문명.cc
+++ C++17/백준/Platinum/14868. 문명/문명.cc
@@ -1,8 +1,7 @@
 //문명
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <unordered_set>
+#include <deque>
 using namespace std;
 
 int N, K, mergeCount;
@@ -10,13 +9,13 @@ int dx[4] = {1, -1, 0, 0};
 int dy[4] = {0, 0, 1, -1};
 vector<int> parent;
 vector<vector<pair<int, int>>> map;
-queue<pair<int, int>> q;
+deque<pair<int, int>> q;
 
 void showMap(int T){
-    cout << "T=" << T << '\n';
+    cout << "T=" << T << ", mergeCnt=" << mergeCount << '\n';
     for(int i=1; i<N+1; i++){
         for(int j=1; j<N+1; j++){
-            cout << map[i][j].first;
+            cout << map[i][j].first << ' ';
         }
         cout << '\n';
     }
@@ -28,45 +27,55 @@ int find(int x){
         return x;
     }
 
-    parent[x] = find(parent[x]);
-    return parent[x];
+    return parent[x] = find(parent[x]);
 }
 
-void merge(int p1, int p2){
-    parent[p2] = p1;
+void merge(int x, int y){
+    x = find(x);
+    y = find(y);
+    if(x==y) return;
+    parent[y] = x;
     mergeCount++;
 }
 
 int BFS(){
     int T = 0;
-    while(++T){
-        queue<pair<int, int>> tempQ;
-        while(!q.empty()){
-            int cx = q.front().first; 
-            int cy = q.front().second; q.pop();
+    while(mergeCount<K-1){
+        deque<pair<int, int>> tempQ;
+        for(pair<int, int> c : q){
+            int cx = c.first; 
+            int cy = c.second;
 
             for(int i=0; i<4; i++){
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
-                if(nx<1 || nx>N || ny<1 || ny>N) continue;
-                if(map[nx][ny].first!=0){
-                    int p1 = find(map[cx][cy].first);
-                    int p2 = find(map[nx][ny].first);
-                    if(p1!=p2){
-                        merge(p1, p2);
-                        if(mergeCount==K-1){
-                            if(map[cx][cy].second==map[nx][ny].second) return T-1;
-                            else return T;
-                        }
-                    }
-                    continue;
-                }
+
+                if(nx<1 || nx>N || ny<1 || ny>N || map[nx][ny].first!=0) continue;
+
                 map[nx][ny] = {map[cx][cy].first, T};
-                tempQ.push({nx, ny});
+                tempQ.emplace_back(nx, ny);
             }
         }
+
+        for(pair<int, int> c : tempQ){
+            int cx = c.first; 
+            int cy = c.second;
+
+            for(int i=0; i<4; i++){
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if(nx<1 || nx>N || ny<1 || ny>N || map[nx][ny].first==0) continue;
+                
+                merge(map[cx][cy].first, map[nx][ny].first);
+            }
+        }
+
+        T++;
         q=tempQ;
     }
+
+    return T;
 }
 
 int main(){
@@ -78,25 +87,25 @@ int main(){
     parent.resize(K+1);
 
     for(int i=1; i<=K; i++){
-        int x, y; cin >> x >> y;
+        int cx, cy; cin >> cx >> cy;
         parent[i] = i;
-        map[x][y].first = i;
-
-        for(int j=0; j<4; j++){
-            int nx = x+dx[j];
-            int ny = y+dy[j];
-            if(nx<1 || nx>N || ny<1 || ny>N) continue;
-            if(map[nx][ny].first){
-                int p1 = find(map[x][y].first);
-                int p2 = find(map[nx][ny].first);
-                if(p1!=p2){
-                    merge(p1, p2);
-                }
-            }
-        }
-        q.push({x, y});
+        map[cx][cy].first = i;
+        q.emplace_back(cx, cy); 
     }
 
-    cout << (mergeCount==K-1 ? 0 : BFS());
+    for(auto c : q){
+        int cx = c.first;
+        int cy = c.second;
+        for(int i=0; i<4; i++){
+            int nx = cx + dx[i];
+            int ny = cy + dy[i];
+
+            if(nx<1 || nx>N || ny<1 || ny>N || map[nx][ny].first==0) continue;
+
+            merge(map[cx][cy].first, map[nx][ny].first);
+        }
+    }
+
+    cout << BFS();
     return 0;
 }
